24338dfc4d670bbdf46c1089fecab4486be46816
Sudheesh Singanamalla <sudheesh1995@outlook.com>
2017-03-23 16:43:38 +0530

    Adding cardgame base with pair connection, dealer and player modes

---
+++ plugin-cardgame-base/src/main/java/ac/robinson/bettertogether/plugin/base/cardgame/BaseDealerActivity.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2017 The Better Together Toolkit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package ac.robinson.bettertogether.plugin.base.cardgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+
+public class BaseDealerActivity extends AppCompatActivity implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+
+    ImageView mDeckImage, mOpenDeckImage, mDiscardedDeckImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base_dealer);
+
+        mDeckImage = (ImageView) findViewById(R.id.deckImage);
+        mOpenDeckImage = (ImageView) findViewById(R.id.openDeckImage);
+        mDiscardedDeckImage = (ImageView) findViewById(R.id.discardedDeckImage);
+
+        mDeckImage.setImageResource(R.drawable.card_back);
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        mOpenDeckImage.setImageResource(R.drawable.ace_of_diamonds);
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        return false;
+    }
+}
